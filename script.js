d378f090e0573026c09ff800b06096661812caa8
Zhi Yong Han <hanzyong0@gmail.com>
2021-10-26 15:47:23 +0800

    add basic math operators

---
+++ script.js
@@ -0,0 +1,27 @@
+const add = function (a, b) {
+    return a + b;
+};
+
+const subtract = function (a, b) {
+    return a - b;
+};
+
+const multiply = function (a, b) {
+    return a * b;
+};
+
+const divide = function (a, b) {
+    return a / b;
+};
+
+const operate = function (a, operator, b) {
+    if (operator == '+') {
+        return add(a, b);
+    } else if (operator == '-') {
+        return subtract(a, b);
+    } else if (operator == '*') {
+        return multiply(a, b);
+    } else if (operator == '/') {
+        return divide(a, b);
+    };
+};
