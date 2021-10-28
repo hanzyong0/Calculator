b9ba2e31ea2fcab2c455a7ff82576790c41fcfa4
Zhi Yong Han <hanzyong0@gmail.com>
2021-10-28 14:36:57 +0800

    add number buttons functions

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ let operator = null;
 
 // Operate function
 const operate = function (x, y, operator) {
+    x = parseInt(x, 10);
+    y = parseInt(y, 10);
     if (operator === '+') {
         return x + y;
     } else if (operator === '-') {
@@ -26,11 +28,11 @@ const cells = document.querySelectorAll('.cell');
 cells.forEach((cell) => {
     cell.addEventListener('click', (e) => {
         if (isNaN(e.target.value)) {
-            a = e.target.value;
+            a = ' ' + e.target.value + ' ';
         } else {
-            a = parseInt(e.target.value, 10);
+            a = e.target.value;
         };
-        display.textContent += a + ' ';
+        display.textContent += a;
     });
 });
 
@@ -43,26 +45,58 @@ clear.addEventListener('click', () => {
 
 // "=" key operate
 const equals = document.querySelector('#operate');
+
 equals.addEventListener('click', () => {
-    if (operator !== null) {
+    if (x !== null && y !== null && operator !== null) {
         answer = operate(x, y, operator);
         display.textContent = answer;
     } else {
         display.textContent = '0';
-    }
+    };
+    x = null;
+    y = null;
+    operator = null;
 });
 
 // Operator buttons functions
 const operatorButtons = document.querySelectorAll('.operator');
+
 operatorButtons.forEach((btn) => {
     btn.addEventListener('click', (e) => {
         if (operator === null) {
             operator = e.target.value;
-        } else {
+        } else if (operator !== null) {
             answer = operate(x, y, operator);
             display.textContent = answer;
             operator = e.target.value;
+            display.textContent += ' ' + operator + ' ';
+            x = answer;
+            y = null;
         };
     });
 });
 
+// Number buttons functions
+const numberButtons = document.querySelectorAll('.number');
+
+// Function for x
+numberButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (x === null && y === null && operator === null) {
+            x = e.target.value;
+        } else if (x !== null && y === null && operator === null) {
+            x += e.target.value;
+        };
+    });
+});
+
+// Function for y
+numberButtons.forEach((btn) => {
+    btn.addEventListener('click', (e) => {
+        if (x !== null && y === null && operator !== null) {
+            y = e.target.value;
+        } else if (x !== null && y !== null && operator !== null) {
+            y += e.target.value;
+        };
+    });
+});
